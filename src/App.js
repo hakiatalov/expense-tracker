a294fab7b4e89b7c08bf9d84e3c79590eaab8a67
Haki Atalov <haki@hakis-mbp.lan>
2021-09-08 21:58:58 -0400

    adding expense wrapper component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -30,10 +30,7 @@ function App() {
 
   return (
     <div className="App">
-      <ExpenseItem 
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date} />
+      <Expenses items = {expenses} />
     </div>
   );
 }
